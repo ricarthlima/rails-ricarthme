7702f7b86900585389d7e793ac10a4db390eba3f
Ricarth Lima <rrsl@cin.ufpe.br>
2019-01-06 20:18:57 +0000

    adicionadas varias paginas de pessoal

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pessoal#index"
+  
+  # Rotas para páginas de pessoal
+  get "oqfev" => "pessoal#vivi"
+  get "livros" => "pessoal#personal_books"
+  get "desafios" => "pessoal#desafios"
 
   # Rotas /c/ para currículos
-  get "c" => "profissional#index"
-  get "c/:empresa" => "profissional#index"
+  get "profissional" => "profissional#index"
+  get "profissional/:empresa" => "profissional#index"
 
 end
